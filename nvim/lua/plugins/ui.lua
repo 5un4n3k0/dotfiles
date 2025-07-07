25b6f1861b4297807f5c2872c17ef282ae9979d4
sunaneko <sasayoru2290@gmail.com>
2025-07-07 14:48:29 +0900

    ⚙ Add ui settings mainly like airline and nerdtree.

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -13,22 +13,94 @@ return {
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
         toggle_tree()
       end, { noremap = true, silent = true })
-
+      
       require("nvim-tree").setup({
         on_attach = function(bufnr)
-          -- overwrite nvim-tree internal key bind
-          vim.keymap.set("n", "<C-e>", toggle_tree, { buffer = bufnr, noremap = true, silent = true })
-        end,
-        view = {
-         width = 30,
+        local api = require("nvim-tree.api")
+
+        vim.keymap.set("n", "<CR>", api.node.open.edit, {
+          desc = "Open file or folder",
+          buffer = bufnr,
+          noremap = true,
+          silent = true
+        })
+
+      -- open files or directories
+      vim.keymap.set("n", "<C-e>", function()
+        vim.cmd("NvimTreeClose")
+      end, { buffer = bufnr, noremap = true, silent = true })
+      end,
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = false,
+      },
+    })
+  end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "kanagawa",  -- colorscheme
+          component_separators = "|",
+          section_separators = "",
         },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = false,
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
         },
       })
+  end,
+  },
+  {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        mode = "buffers",
+        diagnostics = "nvim_lsp",
+        separator_style = "none",  -- "slant" / "thick" / "thin"
+        show_close_icon = false,
+        show_buffer_close_icons = false,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+          }
+        },
+      },
+    })
+
+    -- key bind --
+    -- normal
+    vim.keymap.set("n", "<Right>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+    vim.keymap.set("n", "<Left>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+    -- insert
+    vim.keymap.set("i", "<Right>", function()
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+      vim.cmd("BufferLineCycleNext")
+    end, { desc = "Next buffer (insert mode)", noremap = true, silent = true })
+
+    vim.keymap.set("i", "<Left>", function()
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+      vim.cmd("BufferLineCyclePrev")
+    end, { desc = "Prev buffer (insert mode)", noremap = true, silent = true })
     end,
   }
 }
