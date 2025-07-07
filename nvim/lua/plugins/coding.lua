3cdad0460e8e03266b890ff4c2e537b246b552f6
sunaneko <sasayoru2290@gmail.com>
2025-07-07 15:56:19 +0900

    📝 Write header comment to all files.

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,1 +1,15 @@
+--[[
+               _ _             
+  ___ ___   __| (_)_ __   __ _ 
+ / __/ _ \ / _` | | '_ \ / _` |
+| (_| (_) | (_| | | | | | (_| |
+ \___\___/ \__,_|_|_| |_|\__, |
+                         |___/ 
+]]--
+
+-- Author: sunaneko
+-- ----------------
+-- Coding plugins download and settings like auto pair, comment.
+
+
 return {}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,1 +1,17 @@
+--[[
+ _           
+| |___ _ __  
+| / __| '_ \ 
+| \__ \ |_) |
+|_|___/ .__/ 
+      |_|    
+--]]
+
+-- Author: sunaneko
+-- ----------------
+-- Download and settings lsp.
+-- Mainly, complection settings is here.
+
+
 return {}
+
