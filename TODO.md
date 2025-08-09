ba3a1a740b7f19268b37e8bbbeca676940d03228
sunaneko <sasayoru2290@gmail.com>
2025-08-09 23:40:35 +0900

    🐚 Add zsh settings.

--- TODO.md
+++ TODO.md
@@ -1,5 +1,13 @@
 # TODO
 
+### 2025/08/09
+
+- Do something about additional plugin manage(install, settings, etc...)
+
+plugins
+
+> zoxide, tre, procs, eza..
+
 ### 2025/08/07
 
 - Add install script to `setup.sh` like pyright, lua require for nvim.
--- zsh/zsh/alias.zsh
+++ zsh/zsh/alias.zsh
@@ -1,1 +1,41 @@
+# ----------------------
+#        _ _           
+#   __ _| (_) __ _ ___ 
+#  / _` | | |/ _` / __|
+# | (_| | | | (_| \__ \
+#  \__,_|_|_|\__,_|___/
+# ----------------------
 
+# --- ls ---
+alias ll="ls -l"
+alias la="ls -a"
+alias ld="ls -ld"
+alias li="ls -li"
+alias lt="ls -t"
+alias lla="ls -la"
+alias lai"ls -lai"
+alias llt="ls -lt"
+alias llat="ls -lat"
+alias lait="ls -ait"
+
+if command -v eza >/dev/null 2>&1; then
+    alias ls="eza"
+    alias lt="eza -snew"
+    alias llt="eza -l -snew"
+    alias llat="eza -la -snew"
+    alias lait="ls -ai -snew"
+fi
+
+# --- git ---
+alias gia="git add"
+alias gic="git commit -m"
+alias gip="git push"
+alias gis="git status"
+alias gib="git branch"
+alias gich="git checkout"
+alias gicl="git clone"
+
+# --- NeoVim ---
+alias v="nvim"
+
+# 
